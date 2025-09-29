ac2c356e26b3b010ebe1bf1d22011f806c2ebf66
annetnyabuto <annetgnyabuto@gmail.com>
2025-09-29 11:16:07 +0300

    debugging

--- app.py
+++ app.py
@@ -14,7 +14,11 @@ import jwt
 migrate = Migrate()
 app = Flask(__name__)
 CORS(app, 
-     origins=['http://localhost:5173', 'http://127.0.0.1:5173'], 
+     origins=[
+         'http://localhost:5173', 
+         'http://127.0.0.1:5173',
+         'https://fund-connect-frontend.onrender.com'
+     ],
      allow_headers=['Content-Type', 'Authorization'],
      methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
      supports_credentials=True)
